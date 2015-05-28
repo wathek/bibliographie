9ccef0e382c325265cbf0c777db64851f7bd67f3
louw2901 <wathek.bellah.loued@usherbrooke.ca>
2015-05-28 16:25:32 -0400

    ajout des couleurs

---
+++ app/src/biblio/ColorService.js
@@ -0,0 +1,97 @@
+(function(){
+	'use strict';
+
+	angular.module('biblio')
+		.service('colorService', ['$q', colorService]);
+
+	/**
+	* Users DataService
+	* Uses embedded, hard-coded data model; acts asynchronously to simulate
+	* remote data service call(s).
+	*
+	* @returns {{loadAll: Function}}
+	* @constructor
+	*/
+	function colorService($q){
+		var colors = [
+			{
+				name: 'gray',
+				css: '.gray',
+			},
+			{
+				name: 'red',
+				css: '.red'
+			},
+			{
+				name: 'pink',
+				css: '.pink'
+			},
+			{
+				name: 'purple',
+				css: '.purple'
+			},
+			{
+				name: 'deep-purple',
+				css: '.deep-purple'
+			},
+			{
+				name: 'indigo',
+				css: '.indigo'
+			},
+			{
+				name: 'blue',
+				css: '.blue'
+			},
+			{
+				name: 'light-blue',
+				css: '.light-blue'
+			},
+			{
+				name: 'cyan',
+				css: '.cyan'
+			},
+			{
+				name: 'teal',
+				css: '.teal'
+			},
+			{
+				name: 'green',
+				css: '.green'
+			},
+			{
+				name: 'light-green',
+				css: '.light-gree'
+			},
+			{
+				name: 'lime',
+				css: '.lime'
+			},
+			{
+				name: 'yellow',
+				css: '.yellow'
+			},
+			{
+				name: 'amber',
+				css: '.amber'
+			},
+			{
+				name: 'orange',
+				css: '.orange'
+			},
+			{
+				name: 'deep-orange',
+				css: '.deep-orange'
+			},
+			{
+				name: 'brown',
+				css: '.brown'
+			},
+			{
+				name: 'blue-grey',
+				css: '.blue-grey'
+			}
+		];
+
+		return colors;
+	}
+})();
