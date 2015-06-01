b5357e915dc2a701c607abca7c70368bb8bdc3e4
Wathek LOUED <w.loued@logicale.ca>
2015-06-01 06:57:44 -0400

    maj

--- app/src/biblio/ReferenceService.js
+++ app/src/biblio/ReferenceService.js
@@ -7,7 +7,10 @@
 	function ReferenceService($q){
 		var references = [
 			{
-				name: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
 				categories: [
 					{
 						name: 'Psychologie',
@@ -19,10 +22,13 @@
 					}
 				],
 				type: 'Article',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 			{
-				name: 'A. Batliner, K. Fischer, R. Huber, J. Spilker, E. Noth, "Desperately Seeking Emotion or: Actors, Wizards and Human Beings", Proceedings of ITRW on Speech and Emotion, Newcastle, Northern Ireland, UK, 2000.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'A. Batliner, K. Fischer, R. Huber, J. Spilker, E. Noth, "Desperately Seeking Emotion or: Actors, Wizards and Human Beings", Proceedings of ITRW on Speech and Emotion, Newcastle, Northern Ireland, UK, 2000.',
 				categories: [
 					{
 						name: 'Sociologie',
@@ -30,10 +36,13 @@
 					}
 				],
 				type: 'Thesis',
-				description: 'description ref2'
+				added_at: 'description ref2'
 			},
 			{
-				name: "Picard R. W., Vyzas E., & Healey J. . “Toward Machine Emotional Intelligence: Analysis of Affective Physiological State”, IEEE Transactions on Patterns Analysis and MachineIntelligence, 23, 1175-1191, 2001.",
+				author: [],
+				year: '',
+				journal: '',
+				title: "Picard R. W., Vyzas E., & Healey J. . “Toward Machine Emotional Intelligence: Analysis of Affective Physiological State”, IEEE Transactions on Patterns Analysis and MachineIntelligence, 23, 1175-1191, 2001.",
 				categories: [
 					{
 						name: 'Psychologie',
@@ -41,31 +50,43 @@
 					}
 				],
 				type: 'Article',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 			{
-				name: 'Davidson R.J., Jackson D.C., Kalin N.H. . “Emotion, plasticity, context and regulation: Perspectives from affective neuroscience”, Psychological Bulletin 126, 890-909, 2000.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'Davidson R.J., Jackson D.C., Kalin N.H. . “Emotion, plasticity, context and regulation: Perspectives from affective neuroscience”, Psychological Bulletin 126, 890-909, 2000.',
 				categories: [],
 				type: 'Chapter',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 			{
-				name: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
 				categories: [],
 				type: 'Book',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 			{
-				name: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
 				categories: [],
 				type: 'Chapter',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 			{
-				name: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
+				author: [],
+				year: '',
+				journal: '',
+				title: 'L. Devillers, I. Vasilescu: “Reliability of Lexical and Prosodic cues in two real-life spoken dialog corpora”, LREC, Lisbonne, May 2004.',
 				categories: [],
 				type: 'Thesis',
-				description: 'description ref1'
+				added_at: 'description ref1'
 			},
 		];
 
