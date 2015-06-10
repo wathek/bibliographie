2eea38fa8b09fdb6f6f6182a68cfdc9d2fb9616f
louw2901 <wathek.bellah.loued@usherbrooke.ca>
2015-06-10 14:18:05 -0400

    suppression des dossiers app/src/references app/src/categories et app/src/users

--- app/src/categories/Category.js
+++
@@ -1,7 +0,0 @@
-(function(){
-	'use strict';
-
-	// Prepare the 'categories' module for subsequent registration of controllers and delegates
-	angular.module('categories', [ 'ngMaterial' ]);
-
-})();
